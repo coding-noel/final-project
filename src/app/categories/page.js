679f9590659231fcb48862f31f9638fa7398630c
coding-noel <codingnoel@gmail.com>
2025-11-28 23:43:31 +0000

    Complete catagories page

--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -7,17 +7,28 @@ export default function Home() {
     <div>
       <div className="p-8">
         <Header h1='Categories'>
-          I think I will put different collections of poems here
+          Browse poem catagories below
         </Header>
         <Main>
-          <div className="px-2 grid gap-4 sm:grid-cols-2 lg:grid-cols-4 py-16">
+          <div className="px-2 grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 py-16">
             {categories.map(({ id, category }, index) => (
               <a
+                  href={"category/" + id}
+                  key={index} 
+                  className='border-2 border-blue-300 p-0.5 hover:shadow-md hover:bg-blue-100 bg-blue-50 shadow'
+              >
+                <div
+                  className="h-full w-full block p-8 rounded bg-white hover:bg-stone-50 border-3 border-blue-200 transition cursor-pointer text-center"
+                >
+                  <h3 className="text-blue-400 text-xl sm:text-lg">{category}</h3>
+                </div>
+              {/* <a
                 key={index}
                 href={"category/" + id}
                 className="p-8 rounded-lg bg-white border-3 border-blue-100 shadow hover:shadow-lg hover:bg-stone-50 transition cursor-pointer text-center"
               >
                 <h3 className="text-blue-400 text-xl">{category}</h3>
+              </a> */}
               </a>
             ))}
           </div>
